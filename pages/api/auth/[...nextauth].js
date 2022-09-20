470085a37af7cc1bf2ebb09971f186dfd73de69a
Moh Afif <mohamadafifcom@gmail.com>
2022-09-20 11:14:51 +0700

    login facebook

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,8 +13,8 @@ export const authOptions = {
         }),
 
         FacebookProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
+            clientId: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
         }),
         // ...add more providers here
     ],
